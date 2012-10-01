16f2ba335d6d3a09446532edab9ba3489faac8a6
shomchak <shomik.chakravarty@gmail.com>
2012-09-30 17:09:03 -0700

    fixed gravity vector

--- n-bodies.py
+++ n-bodies.py
@@ -1,25 +1,31 @@
 import pygame
 import random
 import math
+import pdb
 
-number_of_particles = 2
+number_of_particles = 10
 background_colour = (255,255,255)
 velo_line_colour = (255,0,0)
-force_line_colour = (100,200,200)
+force_line_colour = (100,0,255)
 (width, height) = (1000, 1000)
 drag = 1
 elasticity = 1
 gravity = (math.pi, 0.00)
-G = .7
+G = 1
 
 def addVectors((angle1, length1), (angle2, length2)):
-    x  = math.sin(angle1) * length1 + math.sin(angle2) * length2
-    y  = math.cos(angle1) * length1 + math.cos(angle2) * length2
-    
+    x  = math.cos(angle1) * length1 + math.cos(angle2) * length2
+    y  = math.sin(angle1) * length1 + math.sin(angle2) * length2
+    # print "x=", x
+    # print "y=", y
     angle = math.atan2(y, x)
     length  = math.hypot(x, y)
+    # print "angle=", angle
+    # print "length=", length
+    # cos = a/h sin = o/h tan = o/a = (o/h)*(h/a)
 
     return (angle, length)
+
 
 def distanceVector(a,b):
     x = b.x - a.x
@@ -33,8 +39,11 @@ def gForce(particle):
     for other in my_particles:
         if other != particle:
             gMag = G*other.mass/(distanceVector(particle,other)[1]**2)
+            # print "gMag=", gMag
             gArg = distanceVector(particle,other)[0]
+            # print "gARG=", gArg
             resultant = addVectors(resultant,(gArg, gMag))
+    # print resultant
     return resultant
 
 class Particle():
@@ -50,19 +59,19 @@ class Particle():
 
     def display(self):
         pygame.draw.circle(screen, self.colour, (int(self.x), int(self.y)), self.size, self.thickness)
-        
+            
         vHyp = 30#2.0*self.size*self.speed/5
         vEnd = (self.x+vHyp*math.cos(self.angle), self.y + vHyp*math.sin(self.angle))
         pygame.draw.aaline(screen, velo_line_colour, (self.x, self.y), vEnd)
 
-        f = gForce(self)
+        self.f = gForce(self)
         fHyp = 30#2.0*self.size*f[1]*300
-        fEnd = (self.x+fHyp*math.cos(f[0]), self.y + fHyp*math.sin(f[0]))
+        fEnd = (self.x + fHyp*math.cos(self.f[0]), self.y + fHyp*math.sin(self.f[0]))
         pygame.draw.aaline(screen, force_line_colour, (self.x, self.y), fEnd)
 
     def move(self):
         (self.angle, self.speed) = addVectors((self.angle, self.speed), gravity)
-        (self.angle, self.speed) = addVectors((self.angle, self.speed), gForce(self))
+        (self.angle, self.speed) = addVectors((self.angle, self.speed), self.f)#gForce(self))
         self.x += math.cos(self.angle) * self.speed
         self.y += math.sin(self.angle) * self.speed
         self.speed *= drag
@@ -95,14 +104,14 @@ my_particles = []
 
 for n in range(number_of_particles):
     size = random.randint(10, 20)
-    #x = random.randint(size, width-size)
-    #y = random.randint(size, height-size)
+    x = random.randint(size, width-size)
+    y = random.randint(size, height-size)
 
     # x = random.randint(600,800)
     # y = random.randint(600,800)
 
-    x = 400 + 200*(n)
-    y = 400 + 200*n
+    # x = 400 + 200*(n)
+    # y = 400 + 200*n
 
     particle = Particle((x, y), size)
     particle.speed = random.uniform(.3,1)
@@ -111,6 +120,7 @@ for n in range(number_of_particles):
     my_particles.append(particle)
 
 running = True
+first = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -118,9 +128,19 @@ while running:
 
     screen.fill(background_colour)
 
+    toggle = 1
+    
     for particle in my_particles:
+        #if first:
+        particle.display()
         particle.move()
         particle.bounce()
-        particle.display()
-
+    
+        # if toggle == 1:
+        #     print particle.f[0]*180/math.pi
+        #     toggle = 0
+        # else:
+        #     toggle = 1
+    #pdb.set_trace()
+    first = False
     pygame.display.flip()
